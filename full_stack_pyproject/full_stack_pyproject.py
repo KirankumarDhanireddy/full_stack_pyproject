93314f1045e36fbfada61e9d3addb4faedfbdd6e
kirankumardhanireddy <kirankumardhanireddy@gmail.com>
2025-09-15 16:53:57 +0530

    update heading in index function to use State.label for consistency

--- full_stack_pyproject/full_stack_pyproject.py
+++ full_stack_pyproject/full_stack_pyproject.py
@@ -7,14 +7,14 @@ from rxconfig import config
 
 class State(rx.State):
     """The app state."""
-
+    label = "Hey Dude! This is my Website."
 
 def index() -> rx.Component:
     # Welcome Page (Index)
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
+            rx.heading(State.label, "\n Welcome to Reflex!", size="9"),
             rx.text(
                 "Get started by editing ",
                 rx.code(f"{config.app_name}/{config.app_name}.py"),
